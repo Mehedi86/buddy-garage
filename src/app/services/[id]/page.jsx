8d1711b002f316f7f5e0467acd332ac54d71cfca
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-07-25 22:27:31 +0600

    added: nearly complete details page

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -7,7 +7,6 @@ export default async function page({ params }) {
     const p = await params;
     const serviceCollection = dbConnect("services");
     const service = await serviceCollection.findOne({ _id: new ObjectId(p.id) });
-    console.log(service)
     return (
         <div>
             {/* banner section */}
@@ -34,18 +33,32 @@ export default async function page({ params }) {
             </section>
             <section>
                 <div className='container mx-auto grid grid-cols-12 gap-4'>
+                    {/* left side */}
                     <div className='col-span-8 '>
                         <div className='h-[350px] w-full'>
                             <img src={service.img} className='w-full h-full object-fill rounded' alt="" />
                         </div>
+                        <div className='my-12 space-y-4'>
+                            <h1 className='text-2xl font-bold'>{service.title}</h1>
+                            <p className='text-neutral-400'>{service.description}</p>
+                        </div>
+                        <div className='grid grid-cols-2 gap-4'>
+                            {service.facility.map((singleFacility, idx) =>
+                                <div key={idx} className='p-12 bg-stone-200 rounded-xl space-y-2 border-t-2 border-t-[#FF3C00]'>
+                                    <h1 className='text-xl font-bold'>{singleFacility.name}</h1>
+                                    <p className='text-neutral-400'>{singleFacility.details}</p>
+                                </div>
+                            )}
+                        </div>
                     </div>
+                    {/* right side */}
                     <div className='col-span-4'>
                         <div className='bg-stone-300 p-6 rounded'>
                             <h1 className='text-xl font-bold'>Services</h1>
                             {service.facility.map((singleFacility, idx) =>
                                 <div key={idx} className='flex justify-between items-center p-4 bg-neutral-50 my-4 rounded'>
                                     <p>{singleFacility.name}</p>
-                                    <FaArrowRight size={20} className='text-red-500'/>
+                                    <FaArrowRight size={20} className='text-red-500' />
                                 </div>
                             )}
                         </div>
