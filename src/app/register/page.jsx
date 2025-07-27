ff3cddbb9e6d3de2f1d5db66680d22d9e4180c6e
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-07-27 12:56:54 +0600

    added: responsive signup page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import RegisterForm from "@/components/RegisterForm";
-// import SocialSignin from "@/components/shared/SocialSignin";
+import SocialSignin from "@/components/SocialSignin";
 import Image from "next/image";
 import Link from "next/link";
 import React, { Suspense } from "react";
@@ -8,30 +8,30 @@ import React, { Suspense } from "react";
 
 const SignUpPage = () => {
 
-  
+
 
   return (
-    <div className="container px-24 mx-auto py-24">
-      <div className="grid grid-cols-2 gap-12 items-center">
+    <div className="container px-4 md:px-12 lg:px-24 mx-auto py-12 md:py-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
         <div>
           <Image
             src="/assets/images/login/login.svg"
             height="540"
             width="540"
             alt="login image"
+            className="w-full h-auto"
           />
         </div>
-        <div className="border-2 p-12">
-          <h6 className="text-3xl font-semibold text-primary text-center mb-12">
+        <div className="border-2 p-6 md:p-12">
+          <h6 className="text-2xl md:text-3xl font-semibold text-primary text-center mb-6 md:mb-12">
             Register
           </h6>
-          
-          <RegisterForm/>
+          <RegisterForm />
           <div>
-            <h6 className="my-12 text-center">or sign in with</h6>
-            {/* <SocialSignin /> */}
-            <h6 className="my-12 text-center">
-              Already have account ?{" "}
+            <h6 className="my-6 md:my-12 text-center">or sign in with</h6>
+            <SocialSignin />
+            <h6 className="my-6 md:my-12 text-center">
+              Already have an account?{" "}
               <Link className="text-primary font-semibold" href={"/login"}>
                 Sign In
               </Link>
@@ -40,6 +40,7 @@ const SignUpPage = () => {
         </div>
       </div>
     </div>
+
   );
 };
 
