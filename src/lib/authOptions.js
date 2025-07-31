7ebc80d5b7684b3d4f427a33cd8ea4326ba296a2
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-07-31 22:06:01 +0600

    added:save to db social login information

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -2,6 +2,7 @@ import { loginUser } from "@/app/actions/auth/loginUser";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import dbConnect from "./dbConnect";
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -44,5 +45,21 @@ export const authOptions = {
     ],
     pages: {
         signIn: "/login"
+    },
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log({ user, account, profile, email, credentials })
+            if (account) {
+                const { providerAccountId, provider } = account;
+                const { email: user_email, image, name } = user;
+                const userCollection = dbConnect('user');
+                const isExist = await userCollection.findOne({ providerAccountId });
+                if (!isExist) {
+                    const payload = { providerAccountId, provider, email: user_email, image, name };
+                    await userCollection.insertOne(payload)
+                }
+            }
+            return true
+        }
     }
 }
