a7d402f83a763bbff1fcd7652bdbfdd38dceb2c3
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-02 15:35:30 +0600

    added: insert to the database

--- src/app/api/service/route.js
+++ src/app/api/service/route.js
@@ -1,7 +1,9 @@
+import dbConnect from "@/lib/dbConnect";
 import { NextResponse } from "next/server"
 
 export const POST = async (req) => {
     const body = await req.json();
-    console.log(body)
-    return NextResponse.json({});
+    const bookingCollection = dbConnect('bookingCollection');
+    const result = await bookingCollection.insertOne(body);
+    return NextResponse.json(result);
 }
