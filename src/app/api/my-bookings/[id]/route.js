cb825eb8585bb9439394ed95b278a2714e0a1074
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-04 21:01:02 +0600

    added: update page complete

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server"
 
 export const GET = async (req, { params }) => {
@@ -8,5 +9,19 @@ export const GET = async (req, { params }) => {
     const query = { _id: new ObjectId(p.id) }
     const singleBooking = await bookingCollection.findOne(query)
     return NextResponse.json(singleBooking);
+}
+
+export const PATCH = async (req, { params }) => {
+    const p = await params;
+    const bookingCollection = dbConnect('bookingCollection');
+    const body = await req.json()
+    const query = { _id: new ObjectId(p.id) }
+    const filter = {
+        $set: {...body}
+    }
+    const option = { upsert: true }
+    const updateResponse = await bookingCollection.updateOne(query, filter, option);
+    revalidatePath("/my-bookings")
+    return NextResponse.json(updateResponse)
 }
 
